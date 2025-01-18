b3a4f08b7a8e2cba1ab420c5681c2816645ea682
Jeongseok (JS) Lee <142548112+jeongseok-meta@users.noreply.github.com>
2025-01-17 16:02:14 -0800

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,8 @@
 @echo on
 
-set TORCH_CUDA_ARCH_LIST=7.0;8.0;9.0
+if not "%cuda_compiler_version%" == "None" (
+    set TORCH_CUDA_ARCH_LIST=7.0;8.0;9.0
+)
 
 python -m pip install . -vv
 if errorlevel 1 exit 1
